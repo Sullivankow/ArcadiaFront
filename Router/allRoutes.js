54204e60703ecbee5d3163201d07db986f48e768
SundlyDev <sundly@live.fr>
2024-09-16 15:50:04 +0200

    Changement totale: (Front à part) Fonctionnalité route: OK

---
+++ Router/allRoutes.js
@@ -0,0 +1,16 @@
+import Route from "./Route.js";
+
+
+//On définit nos routes ici
+
+export const allRoutes = [
+
+    new Route("/", "Accueil", "/pages/home.html" ),
+
+
+];
+
+
+
+//Le titre s'affiche comme ceci : Route.titre - websitename
+export const websiteName = "Zoo Arcadia";
