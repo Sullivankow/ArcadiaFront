17ac14533c7075fd149109efb97c949fac2e21ed
SundlyDev <sundly@live.fr>
2024-09-20 14:00:07 +0200

    Ajout responsive menu burger mobile

--- js/script.js
+++ js/script.js
@@ -1,30 +1,8 @@
-// Carousel de navigation//
-
-let slideIndex = 1;
-showSlides(slideIndex);
-
-function changeSlide(n) {
-
-    showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-    
-    let slides = document.getElementsByClassName("Carousel-slide");
-    
-
-    if(n > slides.lenght) {
-        slideIndex = 1;
-    }
-    if (n < 1) {
-        slideIndex = slides.lenght;
-    }
 
 
-    slides[slideIndex - 1].computedStyleMap.display = "block";
-    
-}
+   const menuHamburger = document.querySelector(".menu-hamburger")
+        const navLinks = document.querySelector(".nav-links")
+ 
+        menuHamburger.addEventListener('click',()=>{
+        navLinks.classList.toggle('mobile-menu')
+        })
